37e39be99669ac5be9e2dac6e827f4c9fe90abe5
Guto Andreollo <gutoandreollo@users.noreply.github.com>
2017-07-04 20:21:52 +0100

    import from interface

---
+++ cleaner.js
@@ -0,0 +1,22 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('cleaner');
+ * mod.thing == 'a thing'; // true
+ */
+
+var cleaner = {
+    
+    run: function() {
+        for(var name in Memory.creeps) {
+            if(!Game.creeps[name]) {
+                delete Memory.creeps[name];
+                console.log('Clearing non-existing creep memory:', name);
+            }
+        }
+    }
+}
+
+module.exports = cleaner;
